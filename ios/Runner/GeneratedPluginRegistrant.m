0779d3ec2cf4e892c8faca8cd7b8e9a768b6eebf
Bruce Albert <fshmjl@aliyun.com>
2019-07-27 18:09:08 +0800

    提交项目

---
+++ ios/Runner/GeneratedPluginRegistrant.m
@@ -0,0 +1,14 @@
+//
+//  Generated file. Do not edit.
+//
+
+#import "GeneratedPluginRegistrant.h"
+#import <amap_location/AmapLocationPlugin.h>
+
+@implementation GeneratedPluginRegistrant
+
++ (void)registerWithRegistry:(NSObject<FlutterPluginRegistry>*)registry {
+  [AmapLocationPlugin registerWithRegistrar:[registry registrarForPlugin:@"AmapLocationPlugin"]];
+}
+
+@end
